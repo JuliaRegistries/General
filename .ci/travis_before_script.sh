29100e13a0b87d10088f4fa3a20c9356f0089f7a
Dilum Aluthge <dilum@aluthge.com>
2020-02-07 15:14:47 -0500

    Update travis_before_script.sh

--- .ci/travis_before_script.sh
+++ .ci/travis_before_script.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 sleep 1
 try_count=0
 while [ $try_count != 100 ]; do
-	julia --project=.ci/ -e 'using Pkg; Pkg.instantiate()'
-	if [ $? = 0 ]; then exit 0; fi
-	try_count=`expr $try_count + 1`
-	sleep 1
+    julia --project=.ci/ -e 'using Pkg; Pkg.instantiate()'
+    if [ $? = 0 ]; then exit 0; fi
+    try_count=`expr $try_count + 1`
+    sleep 1
 done
 exit 1
