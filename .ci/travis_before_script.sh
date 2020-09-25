7b5f7577e6fc81137961e2e564ee0c49539b7e20
Dilum Aluthge <dilum@aluthge.com>
2020-09-25 07:50:49 +0000

    Increase the max number of retries in the bounded retry loop

--- .ci/travis_before_script.sh
+++ .ci/travis_before_script.sh
@@ -1,5 +1,5 @@
 try_count=0
-while [ $try_count != 5 ]; do
+while [ $try_count != 9 ]; do
     julia --project=.ci/ -e 'using Pkg; Pkg.instantiate()'
     if [ $? = 0 ]; then exit 0; fi
     try_count = `expr $try_count + 1`
