be4fc30e5fda06126cf3b1b8e7b5c10072075f42
Dilum Aluthge <dilum@aluthge.com>
2021-04-26 19:26:20 -0400

    A much better approach to updating manifests (#35378)

--- .ci/travis_before_script.sh
+++
@@ -1,8 +0,0 @@
-try_count=0
-while [ $try_count != 9 ]; do
-    julia --project=.ci/ -e 'using Pkg; Pkg.instantiate()'
-    if [ $? = 0 ]; then exit 0; fi
-    try_count = `expr $try_count + 1`
-    sleep 20
-done
-exit 1
