124b304a31d8af6d1496ced4b381c3148ac2c73b
Dilum Aluthge <dilum@aluthge.com>
2025-09-01 16:56:52 -0400

    Remove commented-out line

--- .ci/Treecheck/Treecheck.jl
+++ .ci/Treecheck/Treecheck.jl
@@ -130,7 +130,6 @@ function verify_archive_tree_hash(tar_gz::AbstractString, expected_hash::String)
     # tarball, tree hash verification requires that the file can i) be
     # decompressed and ii) is a proper archive.
     calc_hash = try
-        # open(Tar.tree_hash, `$(exe7z()) x $tar_gz -so`)
         Tar.tree_hash(CodecZlib.GzipDecompressorStream(tar_gz))
     catch err
         @warn "unable to decompress and read archive" exception = err
